f6fadcc7b8c7ad8381aa01ba84c59f3d9a6f739a
devEricA <eric.anderson413@gmail.com>
2019-07-03 14:38:33 -0500

    URL and engagement updates

--- defectDojo_engagement_survey/models.py
+++ defectDojo_engagement_survey/models.py
@@ -137,7 +137,7 @@ class Engagement_Survey(models.Model):
 # meant to be an answered survey tied to an engagement
 class Answered_Survey(models.Model):
     # tie this to a specific engagement
-    engagement = models.ForeignKey(Engagement, related_name='engagement',
+    dojo_engagement = models.ForeignKey(Engagement, related_name='engagement',
                                    null=True, blank=False, editable=True,
                                    on_delete=models.CASCADE)
     # what surveys have been answered
--- defectDojo_engagement_survey/urls.py
+++ defectDojo_engagement_survey/urls.py
@@ -13,7 +13,6 @@ if not apps.ready:
 admin.autodiscover()
 
 urlpatterns = [
-    '',
     url(r'^survey$',
         views.survey,
         name='survey'),
