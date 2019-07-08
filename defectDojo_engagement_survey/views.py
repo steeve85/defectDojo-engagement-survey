5fdbcbd0fb4e6f37766d0b9a439368b3c71b3fae
devEricA <eric.anderson413@gmail.com>
2019-07-08 14:51:13 -0500

    Filters Clearance

--- defectDojo_engagement_survey/views.py
+++ defectDojo_engagement_survey/views.py
@@ -345,7 +345,6 @@ def edit_survey_questions(request, sid):
 def survey(request):
     user = request.user
     surveys = Engagement_Survey.objects.all()
-    surveys = SurveyFilter(request.GET, queryset=surveys)
     paged_surveys = get_page_items(request, list(surveys), 25)
     add_breadcrumb(title="All Surveys", top_level=True, request=request)
     return render(request, 'defectDojo-engagement-survey/list_surveys.html',
@@ -359,7 +358,6 @@ def survey(request):
 def questions(request):
     user = request.user
     questions = Question.objects.all()
-    questions = QuestionFilter(request.GET, queryset=questions)
     paged_questions = get_page_items(request, list(questions), 25)
     add_breadcrumb(title="All Questions", top_level=False, request=request)
     return render(request, 'defectDojo-engagement-survey/list_questions.html',
