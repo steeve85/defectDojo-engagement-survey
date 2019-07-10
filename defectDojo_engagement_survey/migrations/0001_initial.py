5fb67a75f23cee0a83e8cf6cb98867f88adad2c8
devEricA <eric.anderson413@gmail.com>
2019-07-10 09:50:17 -0500

    first to latest

--- defectDojo_engagement_survey/migrations/0001_initial.py
+++ defectDojo_engagement_survey/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         ('dojo', '0009_endpoint_remediation'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('contenttypes', '0002_remove_content_type_name'),
-        ('defectDojo_engagement_survey', '__first__'),
+        ('defectDojo_engagement_survey', '__latest__'),
     ]
 
     operations = [
