51fe8664f806f72a98dcce4099d160f66d8b408c
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2019-07-18 14:07:36 -0500

    Update 0001_initial.py

--- defectDojo_engagement_survey/migrations/0001_initial.py
+++ defectDojo_engagement_survey/migrations/0001_initial.py
@@ -93,7 +93,6 @@ class Migration(migrations.Migration):
                 'verbose_name': 'Answered Engagement Survey',
                 'verbose_name_plural': 'Answered Engagement Surveys',
             },
-            bases=('defectDojo_engagement_survey.engagement_survey',),
         ),
         migrations.CreateModel(
             name='Answer',
