6755e03cdeb52d950f801c1e94af533abb29ae06
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-07-22 16:11:37 -0500

    Trying thigs...

--- defectDojo_engagement_survey/views.py
+++ defectDojo_engagement_survey/views.py
@@ -448,8 +448,6 @@ def edit_question(request, qid):
 
     if survey:
         answered = Answered_Survey.objects.filter(survey=survey[0])
-        length = answered.count()
-        raise Exception('sdfgh')
         if answered.count() > 0:
             messages.add_message(request,
                                  messages.ERROR,
