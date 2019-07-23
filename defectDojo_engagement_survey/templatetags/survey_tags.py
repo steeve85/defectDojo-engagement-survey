ad5356a47cc35420ea366c0175ab7d4a8373eb28
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-07-23 13:20:54 -0500

    Trying thigs...

--- defectDojo_engagement_survey/templatetags/survey_tags.py
+++ defectDojo_engagement_survey/templatetags/survey_tags.py
@@ -4,19 +4,15 @@ Created on Feb 18, 2015
 @author: jay7958
 '''
 from django import template
-from django.contrib.auth import get_user_model
 from ..models import Answered_Survey, Engagement_Survey
 
-Users = get_user_model
 register = template.Library()
 
 
 @register.inclusion_tag('defectDojo-engagement-survey/surveys.html')
 def show_surveys(engagement):
     surveys = Answered_Survey.objects.filter(engagement=engagement)
-    users = Users.objects.all()
-    return {'surveys': surveys,
-            'users': users}
+    return {'surveys': surveys}
 
 
 @register.inclusion_tag('defectDojo-engagement-survey/add_surveys.html')
